b50ecb5e35af1259da047a6fc86d8533cb939894
Ahmadali <ahmadali.azizov@outlook.com>
2018-07-20 23:20:21 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,13 +3,13 @@ def fizzbuzz(int)
   fizz_5 = 5
   fizz_15 = 15
   fizz_4 = 4
-  if int % fizz_3
+  if int % fizz_3 == 0
     "Fizz"
-    elsif int % fizz_5
+    elsif int % fizz_5 == 0
     "Buzz"
-    elsif int % fizz_15
+    elsif int % fizz_15 && int % fizz_5 == 0
     "FizzBuzz"
-    else int % fizz_4
+    else int % fizz_4 == 0
     nil
   end
 end
