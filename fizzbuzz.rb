d677e99286794569eb7225a1c7ad4a44bfc51a92
Ahmadali <ahmadali.azizov@outlook.com>
2018-07-20 22:38:34 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,11 @@
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
+  if int % 3 == 0 
+    "Fizz"
+    elsif int % 5 == 0
+    "Buzz"
+    elsif int % 3, 5 == 0
+    "FizzBuzz"
+  else 
+    return nil
   end
 end
